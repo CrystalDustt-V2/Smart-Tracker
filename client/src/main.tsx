b8696e1c734c12c3fbc117a086f709f9bc87f8ef
CrystalDustt <crystal6572@gmail.com>
2025-09-28 15:08:21 +0800

    fix 3

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,12 @@
-import { HashRouter } from "react-router-dom";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Router } from "wouter";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-    <HashRouter>
-        <App />
-    </HashRouter>
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+        <Router base="/Smart-Tracker">
+            <App />
+        </Router>
+    </React.StrictMode>
 );
