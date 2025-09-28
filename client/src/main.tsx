3f488be343f20702f13b7f9f3b8be022fbafa629
CrystalDustt <crystal6572@gmail.com>
2025-09-28 15:01:53 +0800

    fix 2

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,9 @@
-import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <HashRouter>
+        <App />
+    </HashRouter>
+);
